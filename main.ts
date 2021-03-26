add94b8ca6a9f2b7cc358e45fd0ea7a83eb19226
Dave Chan <davechan@users.noreply.github.com>
2021-03-27 00:12:44 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,16 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    basic.showNumber(receivedNumber)
     value = receivedNumber
 })
 let value = 0
 radio.setGroup(1)
 value = 0
 basic.forever(function () {
+    led.plotBarGraph(
+    value,
+    100
+    )
+    if (value == 0) {
+        motorbit.brake()
+    }
     motorbit.freestyle(value, value)
 })
